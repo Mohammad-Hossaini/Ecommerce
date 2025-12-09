85857af735fc706a8150030dbc134ff8a5af4da9
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-12-09 02:45:41 -0800

    solved the primary color

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 function Navbar() {
   return (
-    <div>
+    <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white">
+      {" "}
       {/* Upper Navbar */}
-      <div className="bg-[rgb(var(--color-primary)/0.4)] py-3">
+      <div className="bg-primary py-2">
         <div className="container flex justify-between items-center">
           <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
             <img src="./logo.png" alt="Logo" className="w-10" />
@@ -18,7 +19,6 @@ function Navbar() {
           </div>
         </div>
       </div>
-
       {/* Lower Navbar */}
       <div></div>
     </div>
