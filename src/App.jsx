3068a6117aa4ba8ef5630aa162cf4040ee261848
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-12-09 02:26:54 -0800

    bg pronlem

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import Navbar from "./components/navbar/Navbar";
+import "./index.css";
 export default function App() {
-  return <h1 className="">Hello world!</h1>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+function Navbar() {
+  return (
+    <div>
+      {/* Upper Navbar */}
+      <div className="bg-[rgb(var(--color-primary)/0.4)] py-3">
+        <div className="container flex justify-between items-center">
+          <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+            <img src="./logo.png" alt="Logo" className="w-10" />
+            Shpify
+          </a>
+
+          <div className="group">
+            <input
+              type="text"
+              placeholder="Search"
+              className="w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-primary"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Lower Navbar */}
+      <div></div>
+    </div>
+  );
+}
+
+export default Navbar;
