f40a41fb7f6a06b8f78c1e7124a662241d05cf7b
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-12-11 13:51:52 -0800

    Completed the top main Testimonia

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import Bunner from "./components/bunner/Bunner";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Products from "./components/products/Products";
+import Subscribe from "./components/subscription/Subscription";
+import Testimonials from "./components/testimonial/Testimonial";
 import TopProducts from "./components/top products/TopProducts";
 import "./index.css";
-import Subscribe from "./components/subscription/Subscription";
 export default function App() {
   React.useEffect(() => {
     AOS.init({
@@ -25,7 +26,8 @@ export default function App() {
       <Products />
       <TopProducts />
       <Bunner />
-      <Subscribe/>
+      <Subscribe />
+      <Testimonials />
     </div>
   );
 }
