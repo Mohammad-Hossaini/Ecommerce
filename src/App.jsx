7d085efb885c8144d75997f66979dfb0aa6f4850
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-12-11 14:49:54 -0800

    Completed the top main Popup

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import Bunner from "./components/bunner/Bunner";
 import Footer from "./components/footer/Footer";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Popup from "./components/popup/Popup";
 import Products from "./components/products/Products";
 import Subscribe from "./components/subscription/Subscription";
 import Testimonials from "./components/testimonial/Testimonial";
 import TopProducts from "./components/top products/TopProducts";
 import "./index.css";
 export default function App() {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
   React.useEffect(() => {
     AOS.init({
       offset: 100,
@@ -22,7 +27,7 @@ export default function App() {
   });
   return (
     <div>
-      <Navbar />
+<Navbar handleOrderPopup={handleOrderPopup} />
       <Hero />
       <Products />
       <TopProducts />
@@ -30,6 +35,7 @@ export default function App() {
       <Subscribe />
       <Testimonials />
       <Footer />
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
   );
 }
