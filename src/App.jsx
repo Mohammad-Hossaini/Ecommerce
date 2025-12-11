c4be32d29af7e37b49c29eff974119b2b040e02d
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-12-11 12:38:14 -0800

    Implemented the AOS animation to the contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import AOS from "aos"; // <-- default import
+import "aos/dist/aos.css";
+import React from "react";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Products from "./components/products/Products";
 import "./index.css";
 export default function App() {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-out",
+      delay: 100,
+    });
+    AOS.refresh();
+  });
   return (
     <div>
       <Navbar />
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,3 +1,5 @@
+import { FaStar } from "react-icons/fa";
+
 const ProductsData = [
   {
     id: 1,
@@ -48,24 +50,35 @@ function Products() {
         {/* Header Section */}
         <div className="text-center mb-10 max-w-[600px] mx-auto">
           <p className="text-sm text-primay">Top Selling Products for you!</p>
-          <h1 className="text-3xl font-bold">Products</h1>
-          <p className="text-xs text-gray-400">
+          <h1 data-aos= "fade-up" className="text-3xl font-bold">Products</h1>
+          <p data-aos= "fade-up" className="text-xs text-gray-400">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur
             dolore, adipisci laboriosam officiis provident natus?
           </p>
         </div>
         {/* Body section */}
         <div>
-          <div className="">
+          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 place-items-center">
             {/* card section */}
             {ProductsData.map((data) => {
               return (
-                <div key={data.id}>
+                <div
+                data-aos= "fade-up"
+                data-aos-delay={data.aosDelay}
+                 key={data.id} className="space-y-3">
                   <img
                     src={data.img}
                     alt=""
                     className="h-[220px] w-[150px] object-cover rounded-md"
                   />
+                  <div>
+                    <h3 className="font-semibold">{data.title}</h3>
+                    <p className="text-sm text-gray-600">{data.color}</p>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <FaStar className="text-yellow-500" />
+                    <span>{data.rating}</span>
+                  </div>
                 </div>
               );
             })}
