e84844479e02bd20f5ff1e9aaa40583fe1ae7fb8
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-12-11 10:30:46 -0800

    updated the product section1

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,3 +1,46 @@
+const ProductsData = [
+  {
+    id: 1,
+    img: "./women/women.png",
+    title: "Women Ethnic",
+    rating: 5.0,
+    color: "white",
+    aosDelay: "0",
+  },
+  {
+    id: 2,
+    img: "./women/women2.jpg",
+    title: "Women western",
+    rating: 4.5,
+    color: "Red",
+    aosDelay: "200",
+  },
+  {
+    id: 3,
+    img: "./women/women3.jpg",
+    title: "Goggles",
+    rating: 4.7,
+    color: "brown",
+    aosDelay: "400",
+  },
+  {
+    id: 4,
+    img: "./women/women4.jpg",
+    title: "Printed T-Shirt",
+    rating: 4.4,
+    color: "Yellow",
+    aosDelay: "600",
+  },
+  {
+    id: 5,
+    img: "./women/women2.jpg",
+    title: "Fashin T-Shirt",
+    rating: 4.5,
+    color: "Pink",
+    aosDelay: "800",
+  },
+];
+
 function Products() {
   return (
     <div className="mt-14 mb-12">
@@ -12,6 +55,22 @@ function Products() {
           </p>
         </div>
         {/* Body section */}
+        <div>
+          <div className="">
+            {/* card section */}
+            {ProductsData.map((data) => {
+              return (
+                <div key={data.id}>
+                  <img
+                    src={data.img}
+                    alt=""
+                    className="h-[220px] w-[150px] object-cover rounded-md"
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
