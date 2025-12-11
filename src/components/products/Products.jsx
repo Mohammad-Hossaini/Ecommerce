4ddbe09fe0bffca4681105725a7532e49c2c56ee
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-12-11 10:06:05 -0800

    updated the product section

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,9 +1,9 @@
 function Products() {
   return (
     <div className="mt-14 mb-12">
-      <div className="container">
+      <div className="text-center">
         {/* Header Section */}
-        <div className="text-center mb-10 max-w-[600px]">
+        <div className="text-center mb-10 max-w-[600px] mx-auto">
           <p className="text-sm text-primay">Top Selling Products for you!</p>
           <h1 className="text-3xl font-bold">Products</h1>
           <p className="text-xs text-gray-400">
